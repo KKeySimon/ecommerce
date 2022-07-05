df57efd30d0c8bebb4fc5d6c2c87fcf29763ae25
KKeySimon <kkeysimon@gmail.com>
2022-07-06 02:33:01 +0800

    connected sanity and set up herobanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="poggles-1">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="little-legends" className="hero-banner-image" />
+        <p className="poggles-1">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="little-legends" className="hero-banner-image" />
         <div>
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
         </div>
         <div className="desc">
           <h5>Description</h5>
-          <p>DESCRIPTION</p>
+          <p>{heroBanner.desc}</p>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,17 @@ import React from 'react';
 import { client } from '../lib/client'
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Poggles of many variations</p>
       </div> 
 
       <div className="products-container">
-        {['Product 1', 'Product 2'].map(
-          (product) => product)}
+        {products?.map((product) => <Product key={product.id} product={product}/>)}
       </div>
 
       <FooterBanner />
@@ -28,6 +26,10 @@ export const getServerSideProps = async () => {
   const products = await client.fetch(query);
   const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
