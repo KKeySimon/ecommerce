4eeadf9fa749227de11b98eaf84909312a09ed2d
KKeySimon <kkeysimon@gmail.com>
2022-07-06 01:37:26 +0800

    pog

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { client } from '../lib/client'
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = () => {
+  return (
+    <>
+      <HeroBanner />
+
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Poggles of many variations</p>
+      </div> 
+
+      <div className="products-container">
+        {['Product 1', 'Product 2'].map(
+          (product) => product)}
+      </div>
+
+      <FooterBanner />
+    </>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+}
+
+export default Home;
